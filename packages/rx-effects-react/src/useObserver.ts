a8e87c443278b0d9020cd1652d27820d7d57ed07
Artyom Chernyaev <leo240532@gmail.com>
2023-01-13 16:15:45 +0300

    feat: easy to use useObserver

--- packages/rx-effects-react/src/useObserver.ts
+++ packages/rx-effects-react/src/useObserver.ts
@@ -1,5 +1,5 @@
-import { useEffect } from 'react';
-import { Observable, Observer, Subscription } from 'rxjs';
+import { useEffect, useLayoutEffect, useRef } from 'react';
+import { Observable, PartialObserver, Subscription } from 'rxjs';
 import { useConst } from './useConst';
 
 /**
@@ -12,28 +12,72 @@ import { useConst } from './useConst';
  *
  * @example
  * ```ts
- * const observer = useCallback((nextValue) => {
+ * useObserver(source$, (nextValue) => {
  *   logger.log(nextValue);
- * }, []);
- * useObserver(source$, observer);
+ * });
  * ```
  */
 export function useObserver<T>(
   source$: Observable<T>,
-  observerOrNext: Partial<Observer<T>> | ((value: T) => void),
+  observerOrNext: PartialObserver<T> | ((value: T) => void),
 ): Subscription {
   const hookSubscriptions = useConst(() => new Subscription());
 
+  const argsRef = useRef<
+    [Observable<T>, PartialObserver<T> | ((value: T) => void)]
+  >([source$, observerOrNext]);
+
+  // Update the latest observable and callbacks
+  // synchronously after render being committed
+  useIsomorphicLayoutEffect(() => {
+    argsRef.current = [source$, observerOrNext];
+  });
+
   useEffect(() => {
     if (!hookSubscriptions.closed) {
-      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-      // @ts-ignore: subscribe cannot infer the right type.
-      const subscription = source$.subscribe(observerOrNext);
+      const input$ = argsRef.current[0];
+
+      const subscription = input$.subscribe({
+        next: (value) => {
+          if (input$ !== argsRef.current[0]) {
+            // stale observable
+            return;
+          }
+          const nextObserver =
+            (argsRef.current[1] as PartialObserver<T>)?.next ||
+            (argsRef.current[1] as ((value: T) => void) | null | undefined);
+          if (nextObserver) {
+            return nextObserver(value);
+          }
+        },
+        error: (error) => {
+          if (input$ !== argsRef.current[0]) {
+            // stale observable
+            return;
+          }
+          const errorObserver = (argsRef.current[1] as PartialObserver<T>)
+            ?.error;
+          if (errorObserver) {
+            return errorObserver(error);
+          }
+        },
+        complete: () => {
+          if (input$ !== argsRef.current[0]) {
+            // stale observable
+            return;
+          }
+          const completeObserver = (argsRef.current[1] as PartialObserver<T>)
+            ?.complete;
+          if (completeObserver) {
+            return completeObserver();
+          }
+        },
+      });
       hookSubscriptions.add(subscription);
       return () => subscription.unsubscribe();
     }
     return undefined;
-  }, [hookSubscriptions, source$, observerOrNext]);
+  }, [hookSubscriptions, source$]);
 
   useEffect(() => {
     return () => hookSubscriptions.unsubscribe();
@@ -41,3 +85,17 @@ export function useObserver<T>(
 
   return hookSubscriptions;
 }
+
+function isBrowser() {
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
+  return typeof window !== 'undefined';
+}
+
+/**
+ * Prevent React warning when using useLayoutEffect on server.
+ */
+const useIsomorphicLayoutEffect = isBrowser()
+  ? useLayoutEffect
+  : /* istanbul ignore next: both are not called on server */
+    useEffect;
